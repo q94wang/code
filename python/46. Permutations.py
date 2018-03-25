9a34c1d461776f71e8e5e595618e7d6e2896bb2e
q94wang <37642697+q94wang@users.noreply.github.com>
2018-03-25 15:37:09 -0400

    Add files via upload

---
+++ python/46. Permutations.py
@@ -0,0 +1,38 @@
+import copy
+
+class Solution(object):
+    
+    def permute_onestep(self, nums_vect_old, new_number):
+
+        if nums_vect_old is None:
+
+            return [[new_number]]
+
+        nums_vect_new = []
+
+        for vect in nums_vect_old:
+
+            for i in range(len(vect) + 1):
+
+                nums_temp = copy.copy(vect)
+                nums_temp.insert(i, new_number)
+                nums_vect_new.append(nums_temp)
+
+        return nums_vect_new    
+    
+    
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) != 0:
+        
+#             elm = nums[-1]
+#             nums_short = nums[:-1]
+
+#             return permute_onestep(permute(nums_short), elm)
+        
+            elm = nums.pop()
+        
+            return self.permute_onestep(self.permute(nums), elm)
