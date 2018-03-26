2827864a4a131f1d471208710e5a058e6f34e791
q94wang <37642697+q94wang@users.noreply.github.com>
2018-03-26 16:06:56 -0400

    Create 46. Permutations.py

---
+++ 46. Permutations.py
@@ -0,0 +1,53 @@
+'''
+46. Permutations
+Given a collection of distinct numbers, return all possible permutations.
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+'''
+
+class Solution(object):
+    
+    def permute_onestep(self, nums_vect_old, new_number):
+
+#         if len(nums_vect_old) == 0:
+
+#             return [[new_number]]
+
+        nums_vect_new = []
+
+        for vect in nums_vect_old:
+
+            for i in range(len(vect) + 1):
+
+                nums_temp = vect.copy()
+                nums_temp.insert(i, new_number)
+                nums_vect_new.append(nums_temp)
+
+        return nums_vect_new
+    
+    
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) != 0:
+        
+#             elm = nums[-1]
+#             nums_short = nums[:-1]
+
+#             return permute_onestep(permute(nums_short), elm)
+        
+            elm = nums.pop()
+        
+            return self.permute_onestep(self.permute(nums), elm)
+        
+        return [[]]
